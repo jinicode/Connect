ffb53a00811f089286db4d062b610e55c6994ebf
jinit <jinit2000@yahoo.com>
2020-07-10 00:44:20 +0530

    auth handled

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,39 @@
 import React, { Fragment, useEffect } from 'react';
-import {BrowserRouter as Router , Route,Switch} from 'react-router-dom';
-import NavBar from './components/layout/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Routes from './components/routing/Routes';
 
-
-import {Provider } from 'react-redux';
+// Redux
+import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
 import './App.css';
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
-const App = () =>{
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
         <Fragment>
-          <NavBar />
+          <Navbar />
           <Switch>
-            <Route exact path="/" component={Landing}/>
-            <Route component={Routes}/>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
           </Switch>
         </Fragment>
       </Router>
     </Provider>
-  )
+  );
 };
+
 export default App;
