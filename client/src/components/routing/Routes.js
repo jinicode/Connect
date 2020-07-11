c17f171e0c9ff18c031609fa01b24d4821f51862
jinit <jinit2000@yahoo.com>
2020-07-11 20:06:55 +0530

    profiles done and working

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -8,8 +8,6 @@ import CreateProfile from "../profile-forms/CreateProfile";
 import EditProfile from "../profile-forms/EditProfile";
 import AddExperience from "../profile-forms/AddExperience";
 import AddEducation from "../profile-forms/AddEducation";
-import Profiles from "../profiles/Profiles";
-import Profile from "../profile/Profile";
 import NotFound from "../layout/NotFound";
 import PrivateRoute from "../routing/PrivateRoute";
 
@@ -20,8 +18,8 @@ const Routes = () => {
       <Switch>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/profiles" component={Profiles} />
-        <Route exact path="/profile/:id" component={Profile} />
+       
+       
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <PrivateRoute exact path="/create-profile" component={CreateProfile} />
         <PrivateRoute exact path="/edit-profile" component={EditProfile} />
