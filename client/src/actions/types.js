08cb3c82b328aefbc99ee6687483979138c4008c
jinit <jinit2000@yahoo.com>
2020-07-13 00:12:00 +0530

    working on posts

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -14,7 +14,14 @@ export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
-
+export const GET_POSTS = 'GET_POSTS';
+export const GET_POST = 'GET_POST';
+export const POST_ERROR = 'POST_ERROR';
+export const UPDATE_LIKES = 'UPDATE_LIKES';
+export const DELETE_POST = 'DELETE_POST';
+export const ADD_POST = 'ADD_POST';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
 
 
 //As I have all the relevant skills require for this internship including Python , Machine Learning , Deep Learning and good knowledge about Neural Networks . I have passed all the assignments of Stanford Machine Learning , Neural Networks course which were reviewed by moderators . I will be able to apply my knowledge to real life project at this internship . I have made many projects on Machine Learning which includes use of ANN , CNN and RNN . I also have explored the field of Computer Vision and Natural Language Processing . I assure that I will give my be
