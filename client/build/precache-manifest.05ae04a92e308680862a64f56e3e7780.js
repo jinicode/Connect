12effdf25338be6a2bce520a7eae1e54c723e2fe
jinit jain <jinit2000@yahoo.com>
2021-02-03 23:22:34 +0530

    add dockerfiles

---
+++ client/build/precache-manifest.05ae04a92e308680862a64f56e3e7780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48079c0e67cd8b02b200ca8244046e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a789d9a10959caf98824",
+    "url": "/static/css/main.9acfc627.chunk.css"
+  },
+  {
+    "revision": "e7de826c640c2960a558",
+    "url": "/static/js/2.807a11ce.chunk.js"
+  },
+  {
+    "revision": "a789d9a10959caf98824",
+    "url": "/static/js/main.92f896dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
